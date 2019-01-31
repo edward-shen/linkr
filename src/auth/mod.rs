68923784edbeb730df823e84f230f509871351e7
Edward Shen <code@eddie.sh>
2019-01-31 14:41:37 -0500

    added interface functions

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,9 +1,40 @@
-
-pub struct TokenKeyPair {
-    pub token: String,
-    pub key: String,
-}
-
+/// An identity provider must provide authentication for every end point
 pub trait IdentityProvider {
-    fn authenticate_user(user: String, password: String) -> Option<TokenKeyPair>;
+    /// Checks whether or not the user can log in and get a token-key pair
+    fn can_get_token_key_pair(user: String, password: String) -> bool;
+
+    // Basic, core functionality.
+
+    fn can_create_mapping(token: String) -> bool;
+
+    fn can_delete_own_mapping(token: String) -> bool;
+
+    fn can_view_own_mappings(token: String) -> bool;
+
+    fn can_view_own_mapping_stats(token: String) -> bool;
+
+    fn can_request_new_token(token: String) -> bool;
+
+    fn can_delete_token(token: String) -> bool;
+
+    // Administrative tasks
+
+    fn can_toggle_anonymous_mode(token: String) -> bool;
+    
+    fn can_toggle_registration(token: String) -> bool;
+    
+    fn can_delete_users(token: String) -> bool;
+
+    fn can_delete_tokens(token: String) -> bool;
+
+
+    // Moderator tasks
+    
+    /// Checks whether or not the user can get all mappings
+    fn can_view_all_mappings(token: String) -> bool;
+
+    fn can_view_all_mapping_stats(token: String) -> bool;
+
+    fn can_delete_others_mapping(token: String) -> bool;
+
 }
